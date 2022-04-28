92407b0867c8c2bc268cd36ab54162bb7c91d5b6
perinze <perinzefaper@foxmail.com>
2022-04-28 14:49:42 +0800

    done

--- To_Upper.s
+++ To_Upper.s
@@ -17,8 +17,7 @@ To_Upper:
     Write the equivalent C code to this function here (in the comment block)
 
 void To_Upper(char *str) {
-    char *p;
-    for (p = str; *p != 0; p++)
+    for (char *p = str; *p; p++)
         if (*p >= 97 && *p <= 122)
             *p -= 32;
 }
@@ -51,30 +50,32 @@ void To_Upper(char *str) {
 
 
 # Body of function
-begin:
-    movq    %rdi, %rax
-    movzbl  (%rdi), %ecx
-    testb   %cl, %cl
-    je      end_reached
+begin:                          # function begin
+    movq    %rdi, %rax          # move parameter str to %rax
+    movzbl  (%rdi), %ecx        # move first char to %cl
+    testb   %cl, %cl            # test if first char is '\0'
+    je      end_reached         # if so, end reached
 
-end_not_reached:
-    leal    -97(%rax), %edx
-    cmpb    $25, %dl
-    ja      not_lower
-    subl    $32, %ecx
-    addq    $1, %rax
-    movb    %cl, -1(%rax)
-    movzbl  (%rax), %ecx
-    testb   %cl, %cl
-    jne     end_not_reached
+end_not_reached:                # else it goes here
+    leal    -97(%rcx), %edx     # let %dl = %cl - 'a' = %cl - 97. %dl is a copy of %cl used to detect its case
+    cmpb    $25, %dl            # (a trick) for unsigned int, if %dl < 'a', then %dl - 'a' will be a large number
+                                # and %dl > 'z' is equivalent to %dl - 'a' > 25
+    ja      not_lower           # so if either of them is true, i.e. %dl is not lower char, then unsigned %dl - 97
+                                # should be greater than 25
+    subl    $32, %ecx           # if not jumped, %cl is lower char. To upper it, subtract it with 32
+    addq    $1, %rax            # make str pointer increase by 1 to point to the next char
+    movb    %cl, -1(%rax)       # move previous uppered char back to memory
+    movzbl  (%rax), %ecx        # move next char to %cl
+    testb   %cl, %cl            # and test if it's '\0'
+    jne     end_not_reached     # if not '\0', then end is not reached, repeat
 
-not_lower:
-    movzbl  1(%rax), %ecx
-    addq    $1, %rax
-    testb   %cl, %cl
-    jne     end_not_reached
+not_lower:                      # not lower char goes here
+    movzbl  1(%rax), %ecx       # move next char to %cl
+    addq    $1, %rax            # and increase the str pointer by 1
+    testb   %cl, %cl            # test if the next char is '\0'
+    jne     end_not_reached     # if not '\0', then end is not reached, repeat
 
-end_reached:
+end_reached:                    # end reached, will return
     # recover, add later
 # Epilogue
     ret
--- driver.c
+++ driver.c
@@ -1,15 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define RAND (rand())
+#define PRINTABLE 95
 
+void Rand_Str(int n, char *str);
 void To_Upper(char *str);
+void To_Upper_C(char *str);
   
 int main() {
-    char str[20];
-    strcpy(str, "cs 354 is Awesome!");
-    printf("Original string:   %s\n",str);
-    To_Upper(str);
-    printf("\n");
-    printf("Upper case string: %s\n",str);
+    srand(time(NULL));
+    char str[BUFSIZ];
+    char tmp[2][BUFSIZ];
+    for (;;) {
+        memset(str, 0, sizeof(str));
+        memset(tmp, 0, sizeof(tmp));
+        // strcpy(str, "cs 354 is Awesome!");
+        int l = RAND % BUFSIZ;
+        Rand_Str(l, str);
+        strcpy(tmp[1], str);
+        strcpy(tmp[2], str);
+        // printf("str:\n%s\n\ntmp1:\n%s\n\ntmp2:\n%s\n\n", str, tmp[1], tmp[2]);
+        // printf("Original string:   %s\n",str);
+        To_Upper(tmp[1]);
+        To_Upper_C(tmp[2]);
+        if (strcmp(tmp[1], tmp[2])) {
+            printf("err:\nstr:\n%s\n\ntmp1:\n%s\n\ntmp2:\n%s\n\n", str, tmp[1], tmp[2]);
+            exit(1);
+        }
+        // printf("\n");
+        // printf("Upper case string: %s\n",str);
+    }
     return 0;
+}
+
+void Rand_Str(int n, char *str) {
+    for (; n--; str++)
+        *str = ' ' + RAND % 95;
+    *str = '\0';
+}
+
+void To_Upper_C(char *str) {
+    for (char *p = str; *p; p++)
+        if (*p >= 'a' && *p <= 'z')
+            *p += 'A' - 'a';
 }
